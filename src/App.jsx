7aeebe90a9bdd3c85338e0a1752363e2326aac39
yuqi <yuqi@test.com>
2023-07-04 11:34:35 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    document.domain = 'vercel.app'
+  }, []);
   return (
     <>
       <div>
